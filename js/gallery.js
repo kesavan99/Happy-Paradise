676a6c38407ad22856c78a9e871fd859fac91483
kesavan99 <kesavanbala99@gmail.com>
2022-03-31 13:23:11 +0530

    first commit

--- js/gallery.js
+++ js/gallery.js
@@ -1,163 +1,42 @@
-
-"use strict"; // Start of use strict
-
-
-// 1. gallery fancybox activator 
-function GalleryFancyboxActivator () {
-  var galleryFcb = $('.fancybox');
-  if(galleryFcb.length){
-    galleryFcb.fancybox({
-      openEffect  : 'elastic',
-      closeEffect : 'elastic',
-      helpers : {
-        media : {}
-      }
-    });
-  }
-}
-
-
-// 2. gallery
-function fleetGallery () {
-	if ($('.mixit-gallery').length) {
-		$('.mixit-gallery').mixItUp();
-	};
-}
-
-//LightBox / Fancybox
-	if($('.lightbox-image').length) {
-		$('.lightbox-image').fancybox({
-			openEffect  : 'fade',
-			closeEffect : 'fade',
-			helpers : {
-				media : {}
-			}
-		});
-	}
-
-// 3. Responsive Video
-function respnsiveVideo () {
-	if ($('.responsive-video-box').length) {
-		$('.responsive-video-box').fitVids();
-	}
-}
-
-// 4. Video Fancybox
-function videoFancybox () {
-	if ($("a.video-fancybox").length) {
-		$("a.video-fancybox").click(function() {
-		    $.fancybox({
-		            'padding'       : 0,
-		            'autoScale'     : false,
-		            'transitionIn'  : 'none',
-		            'transitionOut' : 'none',
-		            'title'         : this.title,
-		            'width'         : 680,
-		            'height'        : 495,
-		            'href'          : this.href.replace(new RegExp("watch\\?v=", "i"), 'v/index.html'),
-		            'type'          : 'swf',
-		            openEffect      : 'elastic',
-				    closeEffect     : 'elastic',
-				    helpers         : {
-				    	media : {}
-				    },
-		            'swf'           : {
-		            	'wmode'             : 'transparent',
-		                'allowfullscreen'   : 'true'
-		            }
-		        });
-
-		    return false;
-		});
-	};
-}
-
-
-	// Scroll to top
-function scrollToTop() {
-    if ($('.scroll-top').length) {
-
-        //Check to see if the window is top if not then display button
-        $(window).scroll(function() {
-            if ($(this).scrollTop() > 200) {
-                $('.scroll-top').fadeIn();
-            } else {
-                $('.scroll-top').fadeOut();
-            }
-        });
-
-        //Click event to scroll to top
-        $('.scroll-top').click(function() {
-            $('html, body').animate({ scrollTop: 0 }, 1500);
-            return false;
-        });
-    }
-}
-
-
-function galleryMasonaryLayout() {
-    if ($('.masonary-layout').length) {
-        $('.masonary-layout').isotope({
-            layoutMode: 'masonry'
-        });
-    }
-
-    if ($('.post-filter').length) {
-        $('.post-filter li').children('span').click(function() {
-            var Self = $(this);
-            var selector = Self.parent().attr('data-filter');
-            $('.post-filter li').children('span').parent().removeClass('active');
-            Self.parent().addClass('active');
-
-
-            $('.filter-layout').isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 500,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
-    }
-
-    if ($('.post-filter.has-dynamic-filter-counter').length) {
-        // var allItem = $('.single-filter-item').length;
-
-        var activeFilterItem = $('.post-filter.has-dynamic-filter-counter').find('li');
-
-        activeFilterItem.each(function() {
-            var filterElement = $(this).data('filter');
-            console.log(filterElement);
-            var count = $('.gallery-content').find(filterElement).length;
-
-            $(this).children('span').append('<span class="count"><b>' + count + '</b></span>');
-        });
-    };
-}
-
-
-
-// instance of fuction while Document ready event	
-jQuery(document).ready(function () {
-	(function ($) {
-			
-		fleetGallery();
-		GalleryFancyboxActivator();
-		respnsiveVideo();
-		videoFancybox();
-		scrollToTop();
-		
-	})(jQuery);
-});
-
-// instance of fuction while Window Load event
-jQuery(window).load(function () {
-	(function ($) {
-		
-		galleryMasonaryLayout();
-	})(jQuery);
-});
-
-
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
